ff530363184000c9de94fc95e5226026cba0539c
Wojtak <mateusz.wojtaczka68@gmail.com>
2021-06-04 21:13:24 +0200

    Custom board size

--- src/main/java/org/example/View.java
+++ src/main/java/org/example/View.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import org.example.piece.Piece;
 import org.example.piece.enums.PieceColor;
 import org.example.piece.enums.PieceType;
@@ -16,29 +15,34 @@ public class View {
     private BorderPane rootPane;
     private GridPane boardPane;
     private Scene mainScene;
+    private static final int BOARD_SIZE = 16;
 
     public View(){
         rootPane = new BorderPane();
         boardPane = new GridPane();
-        boardPane.setMaxSize(700,700);
+        boardPane.setMaxSize(70 * BOARD_SIZE,70 * BOARD_SIZE);
         boardPane.setStyle("-fx-border-color: black; -fx-border-width: 3");
         rootPane.setCenter(boardPane);
         mainScene = new Scene(rootPane,800,800);
         mainScene.setFill(Color.DARKGRAY);
+
+        BoardGeneration();
+    }
+
+    private void BoardGeneration() {
         Piece pawn = null;
         Tile tile;
-
-        for (int row = 1; row<=10; row++) {
-            for (int col =1; col <= 10; col++) {
+        for (int row = 1; row<= BOARD_SIZE; row++) {
+            for (int col = 1; col <= BOARD_SIZE; col++) {
                 if ((row + col) % 2 == 0) {
-                    tile = new Tile(false, col, row,null);
                     if (row<=3) {
                         pawn = new Piece(PieceType.PAWN, PieceColor.WHITE, col, row);
-                    } else if (row>=8) {
+                    } else if (row>=BOARD_SIZE-2) {
                         pawn = new Piece(PieceType.PAWN, PieceColor.BLACK, col, row);
                     }
+                    tile = new Tile(false, col, row, pawn);
                 } else {
-                    tile = new Tile(true, col, row,null);
+                    tile = new Tile(true, col, row, null);
                 }
                 boardPane.add(tile, col, row);
                 if (pawn != null) {
